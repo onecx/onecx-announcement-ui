a12063d7e5b5b87b4abd736ebd8f22a36d83c2a9
markuczy <129275100+markuczy@users.noreply.github.com>
2024-07-01 13:09:58 +0200

    feat: announcement-ui as webcomponent (#122)
    * feat: announcement-ui to webcomponents beggining
    
    * feat: search showing
    
    * feat: new routing approach
    
    * feat: module guard enabled
    
    * feat: routes matching
    
    * feat: moved back empty component
    
    * feat: prepared ui module as webcomponent
    
    * fix: added AngularAuthModule to fix http requests
    
    * feat: all working with webcomponents implementation
    
    * feat: removed old exposed module and component
    
    * feat: use utils from libs
    
    * feat: updated operator
    
    * fix: changed ocxRemoteWebcomponent package
    
    * feat: use new function from libs
    
    * feat: libs upgrade
    
    * fix: removed console.log

--- src/app/announcement/announcement.module.ts
+++ src/app/announcement/announcement.module.ts
@@ -4,9 +4,8 @@ import { FormsModule } from '@angular/forms'
 import { RouterModule, Routes } from '@angular/router'
 
 import { PortalCoreModule } from '@onecx/portal-integration-angular'
-import { addInitializeModuleGuard, InitializeModuleGuard } from '@onecx/angular-integration-interface'
+import { InitializeModuleGuard, addInitializeModuleGuard } from '@onecx/angular-integration-interface'
 import { SharedModule } from '../shared/shared.module'
-
 import { AnnouncementSearchComponent } from './announcement-search/announcement-search.component'
 import { AnnouncementCriteriaComponent } from './announcement-search/announcement-criteria/announcement-criteria.component'
 import { AnnouncementDetailComponent } from './announcement-detail/announcement-detail.component'
