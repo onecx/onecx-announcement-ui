be7e82ffc40680afc6583df736d28a442c558a37
markuczy <martin.kuczynski@capgemini.com>
2024-10-14 14:55:08 +0200

    fix: platform providers

--- src/app/remotes/announcement-list-active/announcement-list-active.component.bootstrap.ts
+++ src/app/remotes/announcement-list-active/announcement-list-active.component.bootstrap.ts
@@ -1,16 +1,22 @@
 import { importProvidersFrom } from '@angular/core'
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
-import { provideAnimations } from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations'
 
 import { bootstrapRemoteComponent } from '@onecx/angular-webcomponents'
 import { AngularAuthModule } from '@onecx/angular-auth'
 
 import { environment } from 'src/environments/environment'
 import { OneCXAnnouncementListActiveComponent } from './announcement-list-active.component'
+import { BrowserModule } from '@angular/platform-browser'
 
 bootstrapRemoteComponent(
   OneCXAnnouncementListActiveComponent,
   'ocx-announcement-list-active-component',
   environment.production,
-  [provideHttpClient(withInterceptorsFromDi()), importProvidersFrom(AngularAuthModule), provideAnimations()]
+  [
+    provideHttpClient(withInterceptorsFromDi()),
+    importProvidersFrom(AngularAuthModule),
+    importProvidersFrom(BrowserModule),
+    importProvidersFrom(BrowserAnimationsModule)
+  ]
 )
