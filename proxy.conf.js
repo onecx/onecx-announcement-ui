cb73674588ac7dfb78a3f993dd4fb943abedd093
Henry Taeschner <henry.taeschner@capgemini.com>
2024-12-09 09:40:47 +0100

    fix: proxy without log

--- proxy.conf.js
+++ proxy.conf.js
@@ -3,7 +3,7 @@
  *   Request proxy to rewrite URLs and prevent CORS errors
  */
 const logFn = function (req, res) {
-  console.log(new Date().toISOString() + `: bypassing ${req.method} ${req.url} `)
+  //console.log(new Date().toISOString() + `: bypassing ${req.method} ${req.url} `)
 }
 const onProxyRes = function (proxyRes, req, res) {
   logFn(req, res)
